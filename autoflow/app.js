2d8fff1ecff28922c9a4d984ba187515d4d688ae
Mike Sierra <letmespellitoutforyou@gmail.com>
2012-11-23 20:17:56 -0500

    flex bug test cases

--- autoflow/app.js
+++ autoflow/app.js
@@ -1,5 +1,7 @@
 var app = new Function();
 
+kill; // rather than wait for it to die...
+
 app.debug = 0;
 
 app.init = function() {
@@ -8,28 +10,27 @@ app.init = function() {
     app.body = document.querySelector('body');
     app.page = document.querySelector('div.page');
     app.template = app.page.innerHTML;
-    app.exclusions = document.querySelectorAll('figure, pre, table, aside').toArray();
+    app.pulls = document.querySelectorAll('figure, pre, table, aside').toArray();
     app.pageCount = 1;
-    app.exclusionCount = 0;
+    app.pullCount = 0;
     app.dbgPageThreshold = 10;
 };
 
 app.fixPage = function() {
     if (!app.debug) return(false);
+    // 2do
 };
 
 app.pageNeedsFix = function() {
     if (!app.debug) return(false);
-    if (! app.exclusions.length) return(false);
-    app.regions = app.flow.getRegionsByContent( app.exclusions[0] )
+    if (! app.pulls.length) return(false);
+    app.regions = app.flow.getRegionsByContent( app.pulls[0] )
     if (! app.regions.length) return(false)
     return(true);
 };
 
 app.run = function() {
-
     while (app.pageNeedsFix()) app.fixPage();
-
     if (app.debug)    return(1);
     //    for (var i = 0; i < app.dbgPageThreshold; i++) {
     while (app.flow.overset) {
@@ -46,19 +47,20 @@ app.addNewPage = function() {
     return(sect);
 };
 
-app.exclude = function() {
-    var content = app.exclusions[0];
+app._exclude = function() {
+    // 1st try
+    var content = app.pulls[0];
     var region;
     var target;
     var regions = app.flow.getRegionsByContent(content);
     if (! regions) return(false);
-    app.exclusionCount++;
+    app.pullCount++;
 
     console.log(regions);
     if (content.classList.contains('wide')) {
         target = app.page.querySelector('div.row:last-of-type');
-        target.style.WebkitFlowFrom = 'excl_' + app.exclusionCount;
-        content.style.WebkitFlowInto = 'excl_' + app.exclusionCount;
+        target.style.WebkitFlowFrom = 'excl_' + app.pullCount;
+        content.style.WebkitFlowInto = 'excl_' + app.pullCount;
         console.log(target);
         console.log(content);
     }
@@ -67,11 +69,10 @@ app.exclude = function() {
         //        target = document.createElement('')
         //app.page.querySelector('div.row:last-of-type');
     }
-
 }
 
 NodeList.prototype.toArray = function() {
-    for(var arr=new Array(),i=0,l=this.length;i<l;i++){arr.push(this[i])}
+    for( var arr=new Array(), i=0, l=this.length; i < l; i++) { arr.push(this[i]) }
     return(arr);
 };
 
