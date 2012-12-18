ef18fc044fc84ada4e34d06eb989f59284f9ba99
Mike Sierra <letmespellitoutforyou@gmail.com>
2012-12-17 21:32:09 -0500

    transform origin

---
+++ animation/anim_check.sh
@@ -0,0 +1,3 @@
+/Users/sierra/sandbox/webplatform/src/wpdget.pl check tutorials/css_animations
+/Users/sierra/sandbox/webplatform/src/wpdget.pl check tutorials/css_transforms
+/Users/sierra/sandbox/webplatform/src/wpdget.pl check tutorials/css_transitions
---
+++ animation/anim_put.sh
@@ -0,0 +1,1 @@
+/Users/sierra/sandbox/webplatform-tools/wpd edit tutorials/css_transforms "`cat css_transforms.txt`" -u Sierra
