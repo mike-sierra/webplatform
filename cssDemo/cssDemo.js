f1b8ab22c73da187977393554d9dcf359563a22e
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-01-08 16:45:42 -0500

    need to fix metaprop

--- cssDemo/cssDemo.js
+++ cssDemo/cssDemo.js
@@ -1,23 +1,30 @@
 var app = new Function();
 
 window.onload = function() {
-    app.initContent();
-    app.initBrowserEngineType();
+    app.comp = new Object; // cache of values to assemble into complex property values
+    app.func = new Object; // 2doc
+
+    setTimeout(function() {
+        app.initContent();
+        app.initBrowserEngineType();
+        app.initInputs();
+    }, 500);
 };
 
 app.initContent = function() {
 
     // populate interface with content from iframe page, then remove
-    // iframe.
+    // the iframe.
 
     try {
-        document.querySelector('#example').innerHTML = frames[0].document.querySelector('section').innerHTML;
-        document.querySelector('#code').innerHTML = frames[0].document.querySelector('pre').innerHTML;
-        document.querySelector('#controls').innerHTML = frames[0].document.querySelector('form').innerHTML;
+        document.querySelector('#demo_topic').innerHTML  = frames[0].document.querySelector('title').innerHTML;
+        document.querySelector('#demo_example').innerHTML  = frames[0].document.querySelector('section').innerHTML;
+        document.querySelector('#demo_controls').innerHTML = frames[0].document.querySelector('form').innerHTML;
+        document.querySelector('#demo_code').innerHTML     = frames[0].document.querySelector('pre').outerHTML;
         document.querySelector('body').removeChild(document.querySelector('iframe'));
     }
-    catch(e) {
-        alert("Can't run locally on this browser.\nTry Safari/Mozilla/Opera.");
+    catch(err) {
+        alert("Can't run locally on this browser.\nTry Mozilla/Opera.");
     }
 };
 
@@ -25,19 +32,171 @@ app.initBrowserEngineType = function() {
 
     // identify browser in order to generate -vendor-prefixed property
     // names: webkit, moz, & o;
-    // PROP: app.browserEngine (browser engine prefix)
+
     var ua = navigator.userAgent;
     if (ua.match(/webkit/i)) {
-         app.browserEngine = 'webkit';
-    }
-    else if (ua.match(/gecko/i)) {
-         app.browserEngine = 'moz';
-    }
-    else if (ua.match(/opera/i)) {
-         app.browserEngine = 'o';
-    }
-    else {
+         app.browserEngine = 'Webkit';
+    } else if (ua.match(/gecko/i)) {
+         app.browserEngine = 'Moz';
+    } else if (ua.match(/opera/i)) {
+         app.browserEngine = 'O';
+    } else if (ua.match(/explorer/i)) {
+         app.browserEngine = 'IE';
+    } else {
          alert("unknown browser: " + ua);
     }
 };
 
+app.initInputs = function() {
+    app.inputs = document.querySelectorAll('input:not(.exclude), select:not(.exclude)');
+    for (var i = 0, l = app.inputs.length; i < l; i++) {
+        // make sure content is wired correctly:
+        app.validateInput(app.inputs[i]);
+        // remember initial value:
+        app.inputs[i].dataset.init = app.inputs[i].value;
+        // reflect initial value in example & code...
+        app.modify(app.inputs[i]);
+        // ...and respond to changes thereafter
+        app.inputs[i].addEventListener('change', function(e) { app.modify(e.target) });
+    }
+}
+
+app.validateInput = function(input) {
+    if (! input.value) console.log("needs default 'value': " + app.inputs[i].outerHTML);
+    if (! input.id) console.log("needs 'id': " + app.inputs[i].outerHTML);
+}
+
+app.trimDecimal = function(n) {
+    return n.replace(/(\.[0-9][0-9])[0-9]+/, "$1");
+}
+
+app.modify = function(input) {
+    var id;              // used for CSSOM property name, or name of property component
+    var value;           // input value
+    var unit;            // if necessary, e.g. %/deg/s/dpi for numeric units
+    var vendor;          // 2do: spawn vendor prefixed syntax?
+    var metaProp;        // property for which current value serves as a
+                         // component, e.g. individual transform functions
+    var tgtProp;         // alternate property name to which to assign complex
+                         // value; e.g. assign gradient values to background-image
+    var tgtExample;      // alternate ID for node to modify
+    var tgtCode;         // alternate ID for code block <span> in which to
+                         // reflect property name/value
+    var cssValue;        // final property value, including units
+    var cssomProp;       // property name used to manipulate example
+    var cssProp;         // property name reflected in code
+    var cssPropVendor;   //
+    var cssomPropVendor; //
+
+    id = input.id;
+
+    // pre-process value:
+
+    if (input.classList.contains('hex')) {
+        // convert to hex string:
+        value = "#" + (input.value * 1).toString(16) + '00000';
+        value = value.replace(/^(#......).+/, "$1");
+    } else if (input.type == 'range') {
+        // sanitize unrounded decimal range input values:
+         value = app.trimDecimal(input.value);
+     } else {
+         value = input.value;
+     }
+
+    // any unit to be appended to property value?
+    unit = input.dataset.unit || '';
+
+    // cache the input value in case complex property values need to
+    // be built from many components:
+    app.comp[id] = value;
+
+    // Is there any need to build a complex 'meta' property?
+    metaProp = input.dataset.metaprop || false;
+
+    // Is there a need to assign the compiled value to an arbitrary
+    // property name? (E.g., gradient values assigned to
+    // background-image)
+    tgtProp = input.dataset.tgtprop || '';
+
+    if (input.classList.contains(app.browserEngine))
+        vendor = app.browserEngine;
+
+    // calculate final property value:
+
+    if (metaProp && app.func[metaProp]) {
+        try {
+            // pass in value in case it can be used as fallback return value:
+            cssValue = app.func[metaProp](value);
+        }
+        catch(err) {
+            console.log("no metaprop function (app.func." + metaProp + ") defined for ID " + id);
+        }
+    }
+    else {
+        cssValue = value + unit;
+    }
+
+//     if (metaProp) {
+//         try {
+//             ;
+//         }
+//         catch(err) {
+//             app.d('NO CONVERSION FUNCTION ASSOCIATED WITH ' + metaProp + " " + id);
+//         }
+//     } else {
+//         cssValue = value;
+//         app.d('');
+//     }
+
+
+
+
+
+
+    // calculate final property names:
+    cssomProp = tgtProp || metaProp || id;
+    cssProp = cssomProp.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+    // calculate vendor-prefixed property name variants:
+    if (vendor) {
+        cssomPropVendor = app.browserEngine + cssomProp.charAt(0).toUpperCase() + cssomProp.slice(1);
+        cssPropVendor = '-' + app.browserEngine.toLowerCase() + '-' + cssProp;
+    }
+
+    //// REFLECT IN EXAMPLE
+
+    // get target example (2do: support alternate example target)
+    tgtExample = document.querySelector(input.dataset.tgtexample || '#demo_example > *');
+
+    try {
+        tgtExample.style[cssomProp] = cssValue;
+        if (vendor) tgtExample.style[cssomPropVendor] = cssValue;
+    }
+    catch(err) {
+        console.log("no example target defined; need #x_" + cssomProp + " element.");
+    }
+
+    //// REFLECT IN CODE:
+
+    try {
+        tgtCode = document.querySelector(input.dataset.tgtcode || ('#c_' + cssomProp ) );
+        tgtCode.textContent = cssProp + ": " + cssValue + ';';
+        if (vendor) tgtCode.textContent += "\n" + cssPropVendor + ": " + cssValue + ';';
+    }
+    catch(err) {
+        console.log("no code target defined; need #c_" + cssomProp + " element.");
+    }
+
+    // console.log("=====================");
+    // console.log("tgtProp: "+ tgtProp);
+    // console.log("metaProp: "+ metaProp);
+    // console.log("id: "+ id);
+    // console.log("vendor: "+ vendor);
+    // console.log("_____________________");
+    // console.log("cssomProp: " + cssomProp);
+    // console.log("cssProp: "+ cssProp);
+    // console.log("cssomPropVendor: " + cssomPropVendor);
+    // console.log("cssPropVendor: "+ cssPropVendor);
+    // console.log("cssValue: "+ cssValue);
+
+
+};
---
+++ cssDemo/cssDemo_sample.js
@@ -0,0 +1,23 @@
+app.func.filter = function() {
+    var v = '';
+    v += app.comp['filter_blur'];
+    return v;
+}
+
+
+// app.f.WebkitBoxShadow = function() {
+//     var v = '';
+//     v += app.p.WebkitBoxShadow_x + 'px ';
+//     v += app.p.WebkitBoxShadow_y + 'px ';
+//     v += app.p.WebkitBoxShadow_blur + 'px ';
+//     v += app.p.WebkitBoxShadow_spread + 'px ';
+//     v += 'rgba(';
+//     v += app.p.WebkitBoxShadow_r + '%, ';
+//     v += app.p.WebkitBoxShadow_g + '%, ';
+//     v += app.p.WebkitBoxShadow_b + '%, ';
+//     v += app.p.WebkitBoxShadow_a + '';
+//     v += ')';
+//     v += app.p.WebkitBoxShadow_inset && ' inset';
+//     return v;
+// }
+// 
