ad915dd14de0397f01c8bfa8033ff62dd8d2edda
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-01-24 15:17:10 -0500

    moving

---
+++ doc/GETanim.sh
@@ -0,0 +1,3 @@
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get tutorials/css_animations > tut_animations.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get tutorials/css_transforms > tut_transforms.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get tutorials/css_transitions > tut_transitions.txt
---
+++ doc/PUTanim.sh
@@ -0,0 +1,3 @@
+/Users/sierra/sandbox/webplatform-tools/wpd edit tutorials/css_animations "`cat tut_animations.txt`" -u Sierra
+/Users/sierra/sandbox/webplatform-tools/wpd edit tutorials/css_transitions "`cat tut_transitions.txt`" -u Sierra
+/Users/sierra/sandbox/webplatform-tools/wpd edit tutorials/css_transforms "`cat tut_transforms.txt`" -u Sierra
