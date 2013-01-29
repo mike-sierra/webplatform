4b5a65723e933b20b6ebeca6b8b61a40d4a1506c
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-01-29 17:31:25 -0500

    virtual reality

---
+++ samples/js/trans_3d_virtual.js
@@ -0,0 +1,36 @@
+var app = new Object();
+
+app.isMobile = false;
+
+app.ry = document.querySelector('#ry');
+app.rx = document.querySelector('#rx');
+
+window.addEventListener('deviceorientation', function(e) {
+    var threshold = 5;
+    app.isMobile = true;
+
+    app.rotY = app.ry.dataset.y * 1;
+    app.rotY += Math.floor(e.gamma);
+
+    app.rotX = app.rx.dataset.x * 1;
+    app.rotX += Math.floor(e.beta);
+
+    (app.rotX < -threshold) && ( app.rotX = -threshold);
+    (app.rotX > threshold)  && ( app.rotX =  threshold);
+
+    app.ry.dataset.y = app.rotY;
+    app.rx.dataset.x = app.rotX;
+
+    app.ry.style.WebkitTransform = 'rotateY(' + app.rotY + 'deg)';
+    app.rx.style.WebkitTransform = 'rotateX(' + (app.rotX * -1) + 'deg)' + ' translateZ(400px)';
+});
+
+setTimeout(function(){
+    if (app.isMobile) return (false);
+    setInterval(function(){
+        app.rotY = app.ry.dataset.y * 1;
+        app.rotY += 3;
+        app.ry.dataset.y = app.rotY;
+        app.ry.style.WebkitTransform = 'rotateY(' + app.rotY + 'deg)';
+    }, 100);
+},1000);
