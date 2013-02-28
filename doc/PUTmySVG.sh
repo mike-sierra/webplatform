bbd00810e75c768cb59ddaab9b4975de42495966
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-02-28 18:21:43 -0500

    overview

--- doc/PUTmySVG.sh
+++ doc/PUTmySVG.sh
@@ -1,5 +1,5 @@
-# /Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_overview' "`cat smarter_svg_overview.txt`" -u Sierra
-/Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_shapes' "`cat smarter_svg_shapes.txt`" -u Sierra
+/Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_overview' "`cat smarter_svg_overview.txt`" -u Sierra
+# /Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_shapes' "`cat smarter_svg_shapes.txt`" -u Sierra
 # /Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_graphics' "`cat smarter_svg_graphics.txt`" -u Sierra
 # /Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_text' "`cat smarter_svg_text.txt`" -u Sierra
 # /Users/sierra/sandbox/webplatform-tools/wpd edit 'svg/tutorials/smarter_svg_filters' "`cat smarter_svg_filters.txt`" -u Sierra
