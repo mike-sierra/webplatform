7f9ac1d7be0c4e31997d48b25da63dac1ecf8215
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-03-04 11:02:49 -0500

    filter app supports matrixes

--- samples/js/svg_filter.js
+++ samples/js/svg_filter.js
@@ -11,6 +11,12 @@ window.onload = function() {
     // populate channel select:
     app.initSelect();
 
+    // associate controls that build complex values:
+    app.initComponents();
+
+    // conditional interfaces
+    app.initPanels();
+
     // EFFECTS
     app.effects = document.querySelectorAll('section#effects > details');
     for (var i = 0, l = app.effects.length; i < l; i++) {
@@ -28,7 +34,7 @@ window.onload = function() {
     app.debug = document.querySelector('#code');
     // GRAPHIC
     app.g = document.querySelector('#graphic');
-}
+};
 
 app.browserSupport = function() {
     var support = 'open' in document.createElement('details');
@@ -42,7 +48,7 @@ app.browserSupport = function() {
         return(false);
     }
     return(true);
-}
+};
 
 app.initSelect = function() {
     var sel = document.querySelectorAll('.channel');
@@ -54,48 +60,107 @@ app.initSelect = function() {
         }
         sel[i].innerHTML = markup;
     }
-}
+};
+
+app.initPanels = function() {
+        // onchange="this.dataset.value = this.value" 
+
+};
+
+app.initComponents = function() {
+
+    // When an attribute requires a series of more than one value,
+    // these are called components. The component 'map' identifies
+    // what other component IDs are associated with the current input.
+    // The 'value' caches the component's value for each input.
+
+    app.component = {};
+    app.component.map = {};
+    app.component.value = {};
+
+    var components;
+    var strings = [
+                   'cmx01 cmx02 cmx03 cmx04 cmx05 cmx06 cmx07 cmx08 cmx09 cmx10 cmx11 cmx12 cmx13 cmx14 cmx15 cmx16 cmx17 cmx18 cmx19 cmx20',
+
+                   'cvmx01 cvmx02 cvmx03 cvmx04 cvmx05 cvmx06 cvmx07 cvmx08 cvmx09'
+
+             ];
+
+    for (i = 0; i < strings.length; i++) {
+        components = strings[i].split(/ +/);
+        for (i2 = 0; i2 < components.length; i2++) {
+            app.component.map[components[i2]] = components;
+        }
+    }
+
+    app.inputs = document.querySelectorAll('input, select');
+    for (var i = 0, l = app.inputs.length; i < l; i++) {
+        if (app.inputs[i].dataset.component) {
+            app.component.value[app.inputs[i].dataset.component] = app.inputs[i].value;
+        }
+    }
+
+};
+
 
 app.d = function(s) {
     app.debug.textContent = s;
-}
+};
 
 app.drop = function(e) {
     e.preventDefault();
     var data = e.dataTransfer.getData("Text");
     var el = document.getElementById(data).cloneNode(true)
+    var inputs = el.querySelectorAll('*[disabled]');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
+    }
     el.dataset.tag = el.id;
     el.removeAttribute('id');
     el.addEventListener('change', app.modify);
     el.addEventListener('click', app.removeEffect);
     e.currentTarget.appendChild(el);
-}
+};
 
 app.removeEffect = function(e) {
     if (e.target.tagName != 'NAV') return(false);
     var node = e.currentTarget;
     node.parentNode.removeChild(node);
     app.assemble();
-}
+};
 
 app.dragover = function(e) {
     e.preventDefault();
-}
+};
 
 app.dragstart = function(e) {
     e.dataTransfer.setData("Text", e.target.id);
-}
+};
 
 app.modify = function(e) {
-    var ctrl = e.currentTarget;
+    var panel = e.currentTarget;
+    var input = e.target;
     var ctrls = e.currentTarget.querySelectorAll('*[data-attr]');
-    ctrl.dataset.markup = '<' + ctrl.dataset.tag;
+    var comp;
+    var myComponents = false;
+    panel.dataset.markup = '<' + panel.dataset.tag;
     for (var i = 0, l = ctrls.length; i < l; i++) {
-        ctrl.dataset.markup += ' ' + ctrls[i].dataset.attr + '="' + ctrls[i].value + '"';
+        if (ctrls[i].dataset.component) {
+            app.component.value[input.dataset.component] = input.value;
+            if (myComponents) break;
+            myComponents = app.component.map[ ctrls[i].dataset.component ];
+            panel.dataset.markup += ' ' + ctrls[i].dataset.attr + '="';
+            for (var inner = 0; inner < myComponents.length; inner++) {
+                panel.dataset.markup += app.component.value[ myComponents[inner]] + ' ';
+            }
+            panel.dataset.markup += '"';
+        } else {
+            panel.dataset.markup += ' ' + ctrls[i].dataset.attr + '="' + ctrls[i].value + '"';
+        }
     }
-    ctrl.dataset.markup += '>';
-    ctrl.dataset.markup += '</' + ctrl.dataset.tag + '>\n';
-}
+    panel.dataset.markup += '>';
+    panel.dataset.markup += '</' + panel.dataset.tag + '>\n';
+};
 
 app.assemble = function(e) {
     var g;
@@ -123,3 +188,30 @@ app.assemble = function(e) {
     app.d(g);
 };
 
+// code for using object as hash key...
+
+function HashTable() {
+    this.hashes = {};
+};
+
+HashTable.prototype = {
+    constructor: HashTable,
+
+    put: function( key, value ) {
+        this.hashes[ JSON.stringify( key ) ] = value;
+    },
+
+    get: function( key ) {
+        return this.hashes[ JSON.stringify( key ) ];
+    }
+};
+
+// var object1 = new Object();
+// var object2 = new Object();
+// 
+// var myHash = new HashTable();
+// 
+// myHash.put(object1, "value1");
+// myHash.put(object2, "value2");
+// 
+// alert(myHash.get(object1), myHash.get(object2)); // I wish that it will print value1 value2
