24ced73bb7a5e485b9c7e2921239b4ad5605d2be
Mike Sierra <letmespellitoutforyou@gmail.com>
2013-03-27 13:35:09 -0400

    linked SVG ref pages

--- doc/GETfilter.sh
+++ doc/GETfilter.sh
@@ -1,12 +1,1 @@
-
-/Users/sierra/sandbox/webplatform/src/wpdget.pl get css/properties/filter > css_properties_filter.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/blur > css_functions_blur.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/brightness > css_functions_brightness.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/contrast > css_functions_contrast.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/drop-shadow > css_functions_drop-shadow.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/grayscale > css_functions_grayscale.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/hue-rotate > css_functions_hue-rotate.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/invert > css_functions_invert.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/opacity > css_functions_opacity.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/saturate > css_functions_saturate.txt
-# /Users/sierra/sandbox/webplatform/src/wpdget.pl get css/functions/sepia > css_functions_sepia.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get tutorials/custom_filters > custom_filters.txt
--- doc/GETmySVG.sh
+++ doc/GETmySVG.sh
@@ -1,1 +1,6 @@
-/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_basics > smarter_svg_basics.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_shapes      > smarter_svg_shapes.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_overview    > smarter_svg_overview.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_graphics    > smarter_svg_graphics.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_filters     > smarter_svg_filters.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_animation   > smarter_svg_animation.txt
+/Users/sierra/sandbox/webplatform/src/wpdget.pl get svg/tutorials/smarter_svg_interaction > smarter_svg_interaction.txt
